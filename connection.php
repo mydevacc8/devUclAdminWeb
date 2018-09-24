2017745e56e4b12ac7c11f9cf3d57202b5849036
Nikolay685 <32335459+Nikolay685@users.noreply.github.com>
2018-09-25 00:36:08 +0100

    1

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:ucldb.database.windows.net,1433; Database = ucldb", "ucldata", "ucl123Data");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    print("Database connection is established");
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+?>
