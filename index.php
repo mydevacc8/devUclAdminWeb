2762182686f6632fb16f88466cc86b334cc572be
Nikolay685 <32335459+Nikolay685@users.noreply.github.com>
2018-09-25 00:08:30 +0100

    sql check

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?> 
+    </body>
 </html>
+
+<?php
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:ucldb.database.windows.net,1433; Database = ucldb", "ucldata", "ucl123Data");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+
+?>
