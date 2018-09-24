8d1d3dd285ad46a1b4963d6132c9b6decdf70e80
Nikolay685 <32335459+Nikolay685@users.noreply.github.com>
2018-09-25 00:23:15 +0100

    try1

--- index.php
+++ index.php
@@ -12,11 +12,17 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:ucldb.database.windows.net,1433; Database = ucldb", "ucldata", "ucl123Data");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    print("Database connection is established");
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
 
+$stmt = $conn->query('SELECT * FROM tours');
+while ($row = $stmt->fetch())
+{
+    echo $row['name'] . "\n";
+}
 
 ?>
