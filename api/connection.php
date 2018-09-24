0e54e3be07af8f944d576c9d972800cadc24825d
Nikolay685 <32335459+Nikolay685@users.noreply.github.com>
2018-09-25 00:44:39 +0100

    5

--- api/connection.php
+++ api/connection.php
@@ -1,8 +1,10 @@
 <?php
-
+include '../debugLog.php';
 try {
     $conn = new PDO("sqlsrv:server = tcp:ucldb.database.windows.net,1433; Database = ucldb", "ucldata", "ucl123Data");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $data = "Database connection is established.";
+    debug_to_console();
     print("Database connection is established.\n");
 }
 catch (PDOException $e) {
