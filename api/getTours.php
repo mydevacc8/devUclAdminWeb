291f024892115e03b0035edacf1082de2af419ec
Nikolay685 <32335459+Nikolay685@users.noreply.github.com>
2018-09-25 01:00:55 +0100

    9

--- api/getTours.php
+++ api/getTours.php
@@ -3,6 +3,17 @@ include 'connection.php';
 $stmt = $conn->query('SELECT * FROM tours');
 while ($row = $stmt->fetch())
 {
-    echo $row['name'] . "\n";
+    $data[] = array(
+        'name' => $row['name']
+    );
 }
+
+if($stmt){
+    $result = "{'success':true, 'data':" . json_encode($data) . "}";
+}
+else{
+    $result = "{'success':false}";
+}
+
+echo $result;
 ?>
