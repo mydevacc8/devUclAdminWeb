d065ac1fab54ac1baadc6afe7bfd729dc6ae1748
Nikolay <mydevacc8@gmail.com>
2018-09-26 18:50:56 +0100

    getEvents added

---
+++ api/getEvents.php
@@ -0,0 +1,36 @@
+<?php
+include 'connection.php';
+if (isset($_GET['event'])) {
+    $eventId = $_GET['event'];
+
+    $stmt = $conn->query('SELECT * FROM events WHERE id='.$eventId);
+    while ($row = $stmt->fetch())
+    {
+        $data[] = array(
+            'id' => $row['id'],
+            'name' => $row['name'],
+            'descritpion' => $row['description']
+        );
+    }
+
+    if($stmt){
+
+        if ($data[0]['id'] == null){
+            $result = '{"success":false}';
+        }else{
+            $result = '{"success":true, "data":' . json_encode($data) . '}';
+        }
+        
+        
+    }
+    else{
+        $result = '{"success":false}';
+    }
+}
+else{
+    $result = '{"success":false}';
+}
+
+
+echo $result;
+?>
