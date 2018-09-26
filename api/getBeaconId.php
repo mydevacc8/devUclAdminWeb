35e8719a4d1c62a0a03e3c055267c72bbe19c047
Nikolay <mydevacc8@gmail.com>
2018-09-26 16:34:08 +0100

    q

--- api/getBeaconId.php
+++ api/getBeaconId.php
@@ -12,7 +12,12 @@ if (isset($_GET['leg'])) {
     }
 
     if($stmt){
-        $result = '{"success":true, "data":' . json_encode($data) . '}';
+        if ($data[0].id == null){
+            $result = '{"success":false}';
+        }else{
+            $result = '{"success":true, "data":' . json_encode($data) . '}';
+        }
+        
     }
     else{
         $result = '{"success":false}';
