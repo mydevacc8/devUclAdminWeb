4254657e7e8aeaa4d1bd79a6cb2b6ae4eac19484
Nikolay <mydevacc8@gmail.com>
2018-09-26 17:58:42 +0100

    getBeaconMajor added

---
+++ api/getBeaconMajor.php
@@ -0,0 +1,34 @@
+<?php
+include 'connection.php';
+if (isset($_GET['beacon'])) {
+    $beaconId = $_GET['beacon'];
+
+    $stmt = $conn->query('SELECT beaconMajor FROM beacons WHERE id='.$beqconId);
+    while ($row = $stmt->fetch())
+    {
+        $data[] = array(
+            'major' => $row['beaconMajor']
+        );
+    }
+
+    if($stmt){
+
+        if ($data[0]['major'] == null){
+            $result = '{"success":false}';
+        }else{
+            $result = '{"success":true, "data":' . json_encode($data) . '}';
+        }
+        
+        
+    }
+    else{
+        $result = '{"success":false}';
+    }
+}
+else{
+    $result = '{"success":false}';
+}
+
+
+echo $result;
+?>
