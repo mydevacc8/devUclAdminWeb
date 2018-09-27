784144931e702905ec2cfd812864ee9e3bc1dd25
Nikolay685 <32335459+Nikolay685@users.noreply.github.com>
2018-09-27 21:23:14 +0100

    9

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <?php
         include 'api/getToursWeb.php';
         $decode = json_decode($result,true);
-        echo $decode[0]['data'][0];
+        echo $decode[0]['data'][1];
     ?>
 </html>
 
